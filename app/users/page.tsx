501bba23c3f9bda9f1c2c31b3f962dd659d0119c
Anthony Ruiz <ntruiz@amazon.com>
2024-07-12 08:25:24 -0700

    feat: added table os users on user page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,16 @@
 import UserForm from "@/components/UserForm";
 import React from "react";
+import DataTable from "./DataTable";
+import prisma from "@/prisma/db";
 
-const Users = () => {
+const Users = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div>
+      P
       <UserForm />
+      <DataTable users={users} />
     </div>
   );
 };
